2c081cf5c3ed7a7017fe50ac9798b749dcdcc620
Satayjit-Biswas <satayjitbiswas2@gmail.com>
2023-09-16 12:45:13 +0600

    Add Roll

--- src/app/modules/academicFaculty/academicFaculty.routes.ts
+++ src/app/modules/academicFaculty/academicFaculty.routes.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { AcademicFacultyController } from './academicFaculty.controller';
 import { AcademicFacultyZodValidation } from './academicFaculty.validation';
@@ -6,6 +8,8 @@ const router = express.Router();
 
 router.post(
   '/create-academicFaculty',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+
   validateRequest(AcademicFacultyZodValidation.create),
   AcademicFacultyController.insertIntoDB
 );
